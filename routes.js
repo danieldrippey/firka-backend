21728c658b0f97e0cc6544dba70e28a32adbbaa7
Daniel Drippey <danieldrippey@gmail.com>
2019-11-07 18:27:57 +0100

    post /login endpoint is ready

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const { check } = require('express-validator');
 const router = require('express').Router();
 const testController = require('./controllers/test-contoller');
 const registerController = require('./controllers/register-controller');
+const loginController = require('./controllers/login-controller');
 
 const checkFields = require('./services/field-validator');
 const validateHeaders = require('./services/validate-headers');
@@ -15,5 +16,11 @@ router.post('/register', [
   check('password').isLength({ min: 1 }),
   validate,
 ], registerController);
+
+router.post('/login', [
+  check('username').isLength({ min: 1 }),
+  check('password').isLength({ min: 1 }),
+  validate,
+], loginController);
 
 module.exports = router;
---
+++ services/login-response-query.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const connection = require('./database-connection');
+
+const responseQuery = (username) => new Promise((resolve, reject) => {
+  const query = 'SELECT id, username, display_name, victories, avatar FROM players WHERE username=?;';
+  connection.query(query, [username], (err, res) => (err ? reject(err) : resolve(res)));
+});
+
+module.exports = { responseQuery };
