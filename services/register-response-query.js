9a4e7548bebda2616c54aa0cbd6e701ab7fa3417
Daniel Drippey <danieldrippey@gmail.com>
2019-11-08 00:46:36 +0100

    updated queries to get more clear responses

--- services/register-response-query.js
+++ services/register-response-query.js
@@ -4,7 +4,7 @@ const connection = require('./database-connection');
 
 const responseQuery = (id) => new Promise((resolve, reject) => {
   const query = 'SELECT id, username, display_name, victories, avatar FROM players WHERE id=?;';
-  connection.query(query, [id], (err, res) => (err ? reject(err) : resolve(res)));
+  connection.query(query, [id], (err, res) => (err ? reject(err) : resolve(res[0])));
 });
 
 module.exports = { responseQuery };
